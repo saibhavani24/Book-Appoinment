d10e267ca8a9544fff2cc85ae5b2ac1f4c7c2c0d
Bhavani <saibhavani24>
2018-08-29 10:17:19 +0530

    removed unused codes

--- src/app/booking-detail/booking-detail.component.spec.ts
+++ src/app/booking-detail/booking-detail.component.spec.ts
@@ -1,7 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { BookingDetailComponent } from './booking-detail.component';
-
 describe('BookingDetailComponent', () => {
   let component: BookingDetailComponent;
   let fixture: ComponentFixture<BookingDetailComponent>;
